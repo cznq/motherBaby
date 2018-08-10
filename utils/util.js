27e47ea74f01c45087f4286c3f8a86ccef41d030
zijun.wan <1518208233@qq.com>
2018-08-10 14:41:13 +0800

    add http

--- utils/util.js
+++ utils/util.js
@@ -18,15 +18,47 @@ const mformatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
-  return [year, month, day].map(mformatNumber).join('-') 
+  return [year, month, day].map(mformatNumber).join('-')
 }
 
 const mformatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+/**
+ * 获取数据模块
+ *
+ * @param    {string}  url                 服务器地址
+ * @param    {string}  callback             回调函数
+ * @param    {Object}  reqbody             请求参数
+ * @returns  void
+ *
+ * @date     2018-08-10
+ * @author   wzj
+ */
+function http(url, callBack, reqbody) {
+  var reqbody = reqbody ? reqbody : {};
+  var dataStr = JSON.stringify({
+      reqbody: reqbody
+  });
+  wx.request({
+    url: url,
+    data: dataStr,
+    method: 'POST',
+    header: {
+      "Content-Type": "json"
+    },
+    success: function (res) {
+      callBack(res.data);
+    },
+    fail: function (error) {
+      console.log(error)
+    }
+  })
+}
 
 module.exports = {
   formatTime: formatTime,
-  mformatTime: mformatTime
+  mformatTime: mformatTime,
+  http:http
 }
